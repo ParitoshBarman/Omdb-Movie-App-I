84d8bec844328df41037f3a9865ab1ed79117a52
Paritosh Barman <barmanpari163@gmail.com>
2024-01-03 19:32:37 +0530

    Added debouncing and click to fill

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ let key = "bce3e291";
 let search = document.querySelector("#search");
 let form = document.querySelector("form");
 let moviedetails = document.querySelector("#moviedetails");
+let searchresults = document.querySelector("#searchresults");
 
+var debouncingId;
 
+// for movie loading Animation
 function lodingFunc(){
     let loadingHtml = `<div id="loding">
         <div class="container">
@@ -15,6 +18,7 @@ function lodingFunc(){
     moviedetails.innerHTML = loadingHtml;
 }
 
+// movie not fount error handeling
 function movieNotfound(){
     let notfoundHtml = `<div id="Fail">
     <div class="container">
@@ -26,9 +30,10 @@ function movieNotfound(){
     moviedetails.innerHTML = notfoundHtml;
 }
 
-
+// display the search result to the web page
 function displayFunc(arr){
     moviedetails.innerHTML = null;
+    searchresults.innerHTML = null;
     console.log(typeof(arr));
     if(arr!=undefined){
     arr.forEach((element, index) => {
@@ -63,11 +68,54 @@ function displayFunc(arr){
 }
 }
 
-
+// add submit event for the final search result
 form.addEventListener("submit", (e)=>{
     e.preventDefault()
     lodingFunc()
     fetch(`https://www.omdbapi.com/?s=${search.value}&apikey=${key}`).then((res)=>res.json()).then((res)=>{
         displayFunc(res.Search);
     });
+});
+
+
+function debouncing(func, delay){
+    if(debouncingId){
+        clearTimeout(debouncingId);
+    }
+    debouncingId = setTimeout(func, delay);
+}
+
+// add the list of debounchin result to the search list
+function addsearchresult(arr){
+    searchresults.innerHTML = null;
+    if(arr!=undefined){
+        arr.forEach((element) => {
+        let singleLine = document.createElement("div");
+        let name = document.createElement("p");
+        let img = document.createElement("img");
+        // name.textContent = search.value;
+        img.src = element.Poster;
+        name.textContent = element.Title;
+        singleLine.append(img, name);
+        searchresults.append(singleLine);
+        singleLine.addEventListener("click",()=>{
+            // console.log(element.Title);
+            search.value = element.Title;
+            searchresults.innerHTML = null;
+            lodingFunc()
+            fetch(`https://www.omdbapi.com/?s=${search.value}&apikey=${key}`).then((res)=>res.json()).then((res)=>{
+        displayFunc(res.Search);
+    });
+        })
+        });
+    }
+}
+
+// apply debouncing technich on search event
+search.addEventListener("input",()=>{
+    debouncing(()=>{
+        fetch(`https://www.omdbapi.com/?s=${search.value}&apikey=${key}`).then((res)=>res.json()).then((res)=>{
+        addsearchresult(res.Search);
+    });
+    }, 1000);
 });
